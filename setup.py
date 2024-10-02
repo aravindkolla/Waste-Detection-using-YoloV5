0633b352dd4b01dc6aa080fc95eb09c437b0e53a
aravindkolla <aravindk5919@gmail.com>
2024-10-02 11:43:04 +0530

    eight commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Aravind Kolla',
+    author_email= 'aravink5919@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
